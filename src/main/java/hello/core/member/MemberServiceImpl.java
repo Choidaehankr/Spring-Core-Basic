26d0a4b82cca4c5f1328b9918bf3a5a3bc1d6ab4
Choidaehan <71701634+Choidaehankr@users.noreply.github.com>
2022-03-28 02:56:13 +0900

    Singleton and Configuration

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,12 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
+
     private final MemberRepository memberRepository;
-    public MemberServiceImpl(MemberRepository memberRepository) { this.memberRepository = memberRepository;}
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     @Override
     public void join(Member member) {
         memberRepository.save(member);
@@ -11,5 +15,10 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    // 테스트용 코드
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
