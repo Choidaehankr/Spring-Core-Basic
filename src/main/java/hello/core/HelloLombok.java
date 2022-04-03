cbea99c657a695f621875b8bcaaff66b58c01384
Daehan Choi <mnnm22@naver.com>
2022-04-03 23:22:00 +0900

    prototype scope -1

--- src/main/java/hello/core/HelloLombok.java
+++ src/main/java/hello/core/HelloLombok.java
@@ -12,7 +12,6 @@ public class HelloLombok {
 
     private String name;
     private int age;
-
     public static void main(String[] args) {
         HelloLombok helloLombok = new HelloLombok();
         helloLombok.setName("choikorea");
