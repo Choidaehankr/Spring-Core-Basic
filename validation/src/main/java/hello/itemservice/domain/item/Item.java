49c2953229d74fbf399ee0a15f086b8dc7e5839b
Daehan Choi <mnnm22@naver.com>
2022-11-28 18:48:07 +0900

    Bean Validation -Basic

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,27 @@
 package hello.itemservice.domain.item;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합을 10000원 이상 입력해주세요.")
 public class Item {
 
     private Long id;
+
+    @NotBlank(message = "공백ㄴㄴ")
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @Max(9999)
+    @NotNull
     private Integer quantity;
 
     public Item() {
