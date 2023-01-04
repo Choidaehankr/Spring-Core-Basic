79abf24d52ec0b763e298996d77cc9b604524813
Daehan Choi <mnnm22@naver.com>
2023-01-05 01:22:19 +0900

    OAuth2.0 설정

---
+++ freelec-springboot-webservice/src/main/java/com/example/freelecspringbootwebservice/config/auth/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.example.freelecspringbootwebservice.config.auth;
+
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+
+public class SecurityConfig {
+
+}
