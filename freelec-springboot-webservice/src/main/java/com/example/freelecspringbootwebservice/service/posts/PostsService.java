2d16db0d6e4e407fb653afdddf99b4137fb6f024
Daehan Choi <mnnm22@naver.com>
2023-01-04 19:09:37 +0900

    study

--- freelec-springboot-webservice/src/main/java/com/example/freelecspringbootwebservice/service/posts/PostsService.java
+++ freelec-springboot-webservice/src/main/java/com/example/freelecspringbootwebservice/service/posts/PostsService.java
@@ -2,9 +2,12 @@ package com.example.freelecspringbootwebservice.service.posts;
 
 import com.example.freelecspringbootwebservice.domain.posts.Posts;
 import com.example.freelecspringbootwebservice.domain.posts.PostsRepository;
+import com.example.freelecspringbootwebservice.web.dto.PostsListResponseDto;
 import com.example.freelecspringbootwebservice.web.dto.PostsResponseDto;
 import com.example.freelecspringbootwebservice.web.dto.PostsSaveRequestDto;
 import com.example.freelecspringbootwebservice.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +31,13 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public PostsResponseDto findById(Long id) {
