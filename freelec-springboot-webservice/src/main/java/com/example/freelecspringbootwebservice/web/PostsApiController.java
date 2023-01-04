3e0077cb9633124837e9fb9723324692896a9a38
Daehan Choi <mnnm22@naver.com>
2023-01-05 00:24:34 +0900

    crud

--- freelec-springboot-webservice/src/main/java/com/example/freelecspringbootwebservice/web/PostsApiController.java
+++ freelec-springboot-webservice/src/main/java/com/example/freelecspringbootwebservice/web/PostsApiController.java
@@ -5,6 +5,7 @@ import com.example.freelecspringbootwebservice.web.dto.PostsResponseDto;
 import com.example.freelecspringbootwebservice.web.dto.PostsSaveRequestDto;
 import com.example.freelecspringbootwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,9 +28,15 @@ public class PostsApiController {
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
+
 }
