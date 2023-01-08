cc7bd7bd4ea9e31ac47df397c59e15cf7996a0e8
Daehan Choi <mnnm22@naver.com>
2023-01-09 01:43:19 +0900

    테스트 코드 에러 수정

--- freelec-springboot-webservice/src/test/java/com/example/freelecspringbootwebservice/web/HelloControllerTest.java
+++ freelec-springboot-webservice/src/test/java/com/example/freelecspringbootwebservice/web/HelloControllerTest.java
@@ -6,20 +6,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.hamcrest.Matchers.is;
+
+import com.example.freelecspringbootwebservice.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
+//@WebMvcTest(controllers = HelloController.class,
+//            excludeFilters = {
+//        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     void hello가_리턴된다() throws Exception {
 
@@ -29,6 +38,7 @@ class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
