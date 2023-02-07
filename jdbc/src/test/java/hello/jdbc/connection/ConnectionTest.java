210a35135ce422cb83fd4e99d2ed4387464e6473
Daehan Choi <mnnm22@naver.com>
2023-02-07 15:24:32 +0900

    DataSource-ConnectionPool

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -2,6 +2,7 @@ package hello.jdbc.connection;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -28,9 +29,24 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
+
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
     }
