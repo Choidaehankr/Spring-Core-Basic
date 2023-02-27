a2ac364eb3a12a0e88eb19d474594645ae7a1026
Daehan Choi <mnnm22@naver.com>
2023-02-28 00:42:58 +0900

    trim사용하여 update문 수정

--- mybatis/src/main/java/example/mybatis/repository/MemberRepository.java
+++ mybatis/src/main/java/example/mybatis/repository/MemberRepository.java
@@ -4,8 +4,11 @@ import example.mybatis.domain.Member;
 import example.mybatis.dto.MemberRequestDto;
 import example.mybatis.dto.MemberResponseDto;
 import java.util.List;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface MemberRepository {
@@ -17,13 +20,30 @@ public interface MemberRepository {
 
     MemberResponseDto findById(Long id);
 
+    @Select("select * from member where id = #{id}")
+    MemberResponseDto findById2(Long id);
+
     MemberResponseDto findByUsername(String username);
+
+    @Select("select * from member where username = #{username}")
+    MemberResponseDto findByUsername2(String username);
 
     void update(MemberRequestDto dto);
 
+    @Update("update member set username=#{username}, age=#{age}, address=#{address} where id=#{id}")
+    void update2(MemberRequestDto dto);
+    void update3(MemberRequestDto dto);
+
+    void update4(MemberRequestDto dto);
     void deleteById(Long id);
+    @Delete("delete from member where id=#{id}")
+    void deleteById2(Long id);
 
     void deleteAll();
+    @Delete("delete from member")
+    void deleteAll2();
 
     List<MemberResponseDto> findAll();
+    @Select("select * from member order by id DESC")
+    List<MemberResponseDto> findAll2();
 }
--- mybatis/src/test/java/example/mybatis/repository/MemberRepositoryTest.java
+++ mybatis/src/test/java/example/mybatis/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import example.mybatis.domain.Member;
 import example.mybatis.dto.MemberRequestDto;
 import example.mybatis.dto.MemberResponseDto;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,73 +18,133 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MemberRepositoryTest {
 
     @Autowired
-    private MemberRepository userRepository;
+    private MemberRepository memberRepository;
 
-
-    @BeforeEach
-    void before() {
-        userRepository.deleteAll();
-    }
+//    @BeforeEach
+//    void before() {
+//        memberRepository.save1(new Member("ChoiKorea", 26, Gender.MAN, "Cheong-ju"));
+//    }
+//    @AfterEach
+//    void after() {
+//        memberRepository.deleteAll();
+//    }
 
     @Test
     @DisplayName("MyBatis 회원가입 테스트-xml")
     void save1() {
-        userRepository.save1(new Member("Gillette", 26, Gender.MAN, "Cheongju"));
+        memberRepository.save1(new Member("ChoiKorea", 26, Gender.MAN, "Cheongju"));
     }
 
     @Test
     @DisplayName("MyBatis 회원가입 테스트")
     void save2() {
-        userRepository.save2(new Member("ChoiKorea", 24, Gender.WOMAN, "Seoul"));
+        memberRepository.save2(new Member("ChoiKorea", 24, Gender.WOMAN, "Seoul"));
     }
 
     @Test
     @DisplayName("findById 테스트")
     void find() {
-        MemberResponseDto dto = userRepository.findById(16L);
-        assertThat(dto.getUsername()).isEqualTo("korea");
-        MemberResponseDto newDto = userRepository.findByUsername("korea");
+        memberRepository.save1(new Member("ChoiKorea", 26, Gender.MAN, "Cheong-ju"));
+        MemberResponseDto dto = memberRepository.findById(33L);
+        assertThat(dto.getUsername()).isEqualTo("ChoiKorea");
+        MemberResponseDto newDto = memberRepository.findByUsername("korea");
         assertThat(newDto.getAge()).isEqualTo(26);
+    }
+    
+    @Test
+    @DisplayName("findById 테스트2")
+    void find2() {
+        MemberResponseDto dto = memberRepository.findById2(33L);
+        assertThat(dto.getUsername()).isEqualTo("ChoiKorea");
+    }
+
+    @Test
+    @DisplayName("findByUsername 테스트2")
+    void find3() {
+        MemberResponseDto dto = memberRepository.findByUsername2("ChoiKorea");
+        assertThat(dto.getAge()).isEqualTo(26);
     }
 
     @Test
     @DisplayName("update 테스트")
     void update() {
         MemberRequestDto dto = MemberRequestDto.builder()
-                .id(17L)
+                .id(47L)
+                .username("Kim")
+                .address("Seoul")
+                .build();
+        memberRepository.update(dto);
+        MemberResponseDto updatedMember = memberRepository.findById(47L);
+        assertThat(updatedMember.getUsername()).isEqualTo(dto.getUsername());
+    }
+
+    @Test
+    @DisplayName("update2 테스트")
+    void update2() {
+        MemberRequestDto dto = MemberRequestDto.builder()
+                .id(33L)
                 .username("kim")
                 .age(33)
                 .address("Seoul")
                 .build();
-        userRepository.update(dto);
-        MemberResponseDto updatedMember = userRepository.findById(17L);
+        memberRepository.update(dto);
+        MemberResponseDto updatedMember = memberRepository.findById(33L);
         assertThat(updatedMember.getUsername()).isEqualTo(dto.getUsername());
+    }
+
+    @Test
+    @DisplayName("update3 테스트-Null 입력 테스트")
+    void update3() {
+        MemberRequestDto dto = MemberRequestDto.builder()
+                .id(47L)
+                .age(26)
+                .build();
+        memberRepository.update3(dto);
+        MemberResponseDto updatedMember = memberRepository.findById(47L);
+        assertThat(updatedMember.getAge()).isEqualTo(dto.getAge());
     }
 
     @Test
     @DisplayName("delete 테스트")
     void delete() {
-        userRepository.deleteById(17L);
-        MemberResponseDto deletedMember = userRepository.findById(17L);
+        memberRepository.deleteById(17L);
+        MemberResponseDto deletedMember = memberRepository.findById(17L);
+        assertThat(deletedMember).isNull();
+    }
+
+    @Test
+    @DisplayName("delete 테스트2")
+    void delete2() {
+        memberRepository.deleteById(48L);
+        MemberResponseDto deletedMember = memberRepository.findById(34L);
         assertThat(deletedMember).isNull();
     }
 
     @Test
     @DisplayName("findAll 테스트")
     void findAll() {
-        userRepository.save1(new Member("Gillette", 26, Gender.MAN, "Cheongju"));
-        userRepository.save2(new Member("ChoiKorea", 24, Gender.WOMAN, "Seoul"));
-        List<MemberResponseDto> memberList = userRepository.findAll();
+        memberRepository.save1(new Member("Gillette", 26, Gender.MAN, "Cheongju"));
+        memberRepository.save2(new Member("ChoiKorea", 24, Gender.WOMAN, "Seoul"));
+        List<MemberResponseDto> memberList = memberRepository.findAll();
+        assertThat(memberList.size()).isEqualTo(2);
+    }
+    @Test
+    @DisplayName("findAll 테스트2")
+    void findAll2() {
+        deleteAll();
+        memberRepository.save1(new Member("Gillette", 26, Gender.MAN, "Cheongju"));
+        memberRepository.save2(new Member("ChoiKorea", 24, Gender.WOMAN, "Seoul"));
+        List<MemberResponseDto> memberList = memberRepository.findAll2();
         assertThat(memberList.size()).isEqualTo(2);
     }
 
     @Test
     @DisplayName("deleteAll 테스트")
     void deleteAll() {
-        userRepository.save1(new Member("Gillette", 26, Gender.MAN, "Cheongju"));
-        userRepository.save2(new Member("ChoiKorea", 24, Gender.WOMAN, "Seoul"));
-        userRepository.deleteAll();
-        List<MemberResponseDto> memberList = userRepository.findAll();
+        memberRepository.save1(new Member("Gillette", 26, Gender.MAN, "Cheongju"));
+        memberRepository.save2(new Member("ChoiKorea", 24, Gender.WOMAN, "Seoul"));
+        memberRepository.deleteAll2();
+        List<MemberResponseDto> memberList = memberRepository.findAll();
         assertThat(memberList.size()).isEqualTo(0);
     }
 }
